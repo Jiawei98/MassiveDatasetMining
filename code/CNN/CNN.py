414f5a54a2968f3a43a66c10b521db4a3f0ddbab
Jiawei98 <70461753+Jiawei98@users.noreply.github.com>
2020-12-06 06:24:47 -0600

    Submitted Condor Queue

---
+++ code/CNN/CNN.py
@@ -0,0 +1,165 @@
+#!/usr/bin/env python
+
+import sys
+import argparse
+import os
+import time
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+import torchvision
+from torchvision import datasets
+from torchvision import transforms
+from PIL import Image
+
+if torch.cuda.is_available():
+    torch.backends.cudnn.deterministic = True
+
+# Get Argument
+hyper_parameters = pd.read_csv("randomSearchCNN.csv").iloc[int(sys.argv[1])].values.tolist()
+lrate = hyper_parameters[6]
+hyper_parameters = [int(i) for i in hyper_parameters]
+## Model Settings
+RANDOM_SEED = 1
+LEARNING_RATE = lrate
+BATCH_SIZE = hyper_parameters[7]
+NUM_EPOCHS = 15
+NUM_FEATURES = 32*32
+NUM_CLASSES = 22
+
+if torch.cuda.is_available():
+    DEVICE = "cuda:0"
+else:
+    DEVICE = "cpu"
+    
+GRAYSCALE = True
+
+## Load Dataset
+resize_transform = transforms.Compose([transforms.Grayscale(),transforms.Resize((32, 32)),transforms.ToTensor()])
+train_dataset = torchvision.datasets.ImageFolder(root="dataset/images_train", 
+                                                 transform=resize_transform)
+test_dataset = torchvision.datasets.ImageFolder(root="dataset/images_validation", 
+                                                transform=resize_transform)
+
+
+train_loader = DataLoader(dataset=train_dataset, 
+                          batch_size=BATCH_SIZE, 
+                          shuffle=True)
+test_loader = DataLoader(dataset=test_dataset, 
+                         batch_size=BATCH_SIZE, 
+                         shuffle=False)
+
+device = torch.device(DEVICE)
+torch.manual_seed(0)
+print("End of loading data.")
+
+## Model
+class LeNet5(nn.Module):
+
+    def __init__(self, num_classes, grayscale=False):
+        super(LeNet5, self).__init__()
+        
+        self.grayscale = grayscale
+        self.num_classes = num_classes
+
+        if self.grayscale:
+            in_channels = 1
+        else:
+            in_channels = 3
+
+        self.features = nn.Sequential(
+            
+            nn.Conv2d(in_channels, hyper_parameters[0], kernel_size=hyper_parameters[1],padding=int((hyper_parameters[1]-1)/2)),
+            nn.Tanh(),
+            nn.MaxPool2d(kernel_size=2),
+            nn.Conv2d(hyper_parameters[0], hyper_parameters[2], kernel_size=hyper_parameters[3],padding=int((hyper_parameters[3]-1)/2)),
+            nn.Tanh(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Linear(int(hyper_parameters[2]*8*8),hyper_parameters[4]),
+            nn.Tanh(),
+            nn.Linear(hyper_parameters[4], hyper_parameters[5]),
+            nn.Tanh(),
+            nn.Linear(hyper_parameters[5], num_classes),
+        )
+
+
+    def forward(self, x):
+        x = self.features(x)
+        x = torch.flatten(x, 1)
+        logits = self.classifier(x)
+        probas = F.softmax(logits, dim=1)
+        return logits, probas
+
+torch.manual_seed(RANDOM_SEED)
+
+model = LeNet5(NUM_CLASSES, GRAYSCALE)
+model.to(DEVICE)
+optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)  
+
+## Model Training
+def compute_accuracy(model, data_loader, device):
+    correct_pred, num_examples = 0, 0
+    for i, (features, targets) in enumerate(data_loader):
+            
+        features = features.to(device)
+        targets = targets.to(device)
+
+        logits, probas = model(features)
+        _, predicted_labels = torch.max(probas, 1)
+        num_examples += targets.size(0)
+        correct_pred += (predicted_labels == targets).sum()
+    return correct_pred.float()/num_examples * 100
+    
+train_acc=np.array([])
+test_acc=np.array([])
+
+start_time = time.time()
+for epoch in range(NUM_EPOCHS):
+    
+    model.train()
+    for batch_idx, (features, targets) in enumerate(train_loader):
+        
+        features = features.to(DEVICE)
+        targets = targets.to(DEVICE)
+            
+        logits, probas = model(features)
+        cost = F.cross_entropy(logits, targets)
+        optimizer.zero_grad()
+        
+        cost.backward()
+        optimizer.step()
+        
+        if not batch_idx % 50:
+            print ('Epoch: %03d/%03d | Batch %04d/%04d | Cost: %.4f' 
+                   %(epoch+1, NUM_EPOCHS, batch_idx, 
+                     len(train_loader), cost))
+
+    model.eval()
+    with torch.set_grad_enabled(False): # save memory during inference
+        acc1=compute_accuracy(model, train_loader, device=DEVICE)
+        acc2=compute_accuracy(model, test_loader, device=DEVICE)
+        train_acc=np.append(train_acc, acc1)
+        test_acc=np.append(test_acc, acc2)
+        print('Epoch: %03d/%03d | Train: %.3f%% | Test: %.3f%%' % (
+              epoch+1, NUM_EPOCHS, acc1, acc2))
+
+    print('Time elapsed: %.2f min' % ((time.time() - start_time)/60))
+    
+print('Total Training Time: %.2f min' % ((time.time() - start_time)/60))
+
+## Model Evaluation
+
+print('Test accuracy: %.2f%%' % (compute_accuracy(model, test_loader, device=DEVICE)))
+
+all_acc = [train_acc[len(train_acc)-1],test_acc[len(test_acc)-1]]
+res = hyper_parameters + all_acc
+
+with open("res_"+sys.argv[1]+".txt", 'w') as f:
+    for item in res:
+        f.write("%s\t" % item)
